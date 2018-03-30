71e75f72d6ecbf7018b4a804195205ff7cfe7100
James Peterson <jpeterson@einsteinmedical.com>
2018-03-30 08:56:25 -0700

    chore: init setup

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const app = express()
+const PORT = process.env.PORT || 8000
+
+app.get('*', (req, res) => {
+
+})
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
