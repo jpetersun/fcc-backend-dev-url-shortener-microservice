f214e914b1e58bb13307468fff5be58c0c7a9e36
James Peterson <jpeterson903@yahoo.com>
2018-03-30 10:13:29 -0700

    feat: shorten urls

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const express = require('express')
+const tinyUrl = require('tinyurl')
 
 const app = express()
 const PORT = process.env.PORT || 8000
 
-app.get('*', (req, res) => {
 
+function getShortUrl (url) {
+  return new Promise((resolve, reject) => {
+    tinyUrl.shorten(url, (shortUrl) => {
+      resolve(shortUrl)
+    })
+  })
+}
+
+app.get('/new/*', (req, res) => {
+  const originalUrl = req.path.slice(5)
+
+  getShortUrl(originalUrl)
+    .then(shortUrl => {
+      const urls = {
+        originalUrl,
+        shortUrl
+      }
+
+      res.json(urls)
+    })
+    .catch(error => res.send(error.message))
 })
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
